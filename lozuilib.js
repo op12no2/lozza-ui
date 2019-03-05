fe3a3220903d070fdbc3614f7de72b39fef267cd
Colin Jenkins <op12no2@gmail.com>
2019-03-05 19:53:07 +0000

    Add files via upload

---
+++ lozuilib.js
@@ -0,0 +1,317 @@
+
+if (typeof console == "undefined") {
+  this.console = {log: function() {}};
+}
+
+var lozData = {};
+
+lozData.next     = 0; // next board id.
+lozData.hashFull = 0;
+lozData.mvNum    = 0;
+lozData.mvStr    = '';
+lozData.source   = 'lozza.js';
+
+//{{{  Number.round
+
+Number.prototype.round = function (places) {
+
+  return +(Math.round(this + "e+" + places)  + "e-" + places);
+}
+
+//}}}
+//{{{  lozGetURLArgs
+
+function lozGetURLArgs() {
+
+  var url = location.href;
+  var qs  = url.substring(url.indexOf('?') + 1).split('&');
+
+  for(var i = 0, result = {}; i < qs.length; i++) {
+    qs[i]            = qs[i].split('=');
+    result[qs[i][0]] = decodeURIComponent(qs[i][1]);
+  }
+
+  return result;
+}
+
+//}}}
+//{{{  lozDecodeFEN
+
+function lozDecodeFEN (fen) {
+
+  fen = fen.replace(/\s+/g,' ');
+
+  var a = fen.split(' ');
+
+  var feno = {};
+
+  feno.board  = (a[0] == undefined) ? ''     : a[0];
+  feno.turn   = (a[1] == undefined) ? 'w'    : a[1];
+  feno.rights = (a[2] == undefined) ? 'KQkq' : a[2];
+  feno.ep     = (a[3] == undefined) ? '-'    : a[3];
+
+  return feno;
+}
+
+//}}}
+//{{{  lozEncodeFEN
+
+function lozEncodeFEN(feno) {
+
+  return feno.board + ' ' + feno.turn  + ' ' + feno.rights  + ' ' + feno.ep + ' 0 1';
+}
+
+//}}}
+//{{{  lozMakeURL
+
+function lozMakeURL(u) {
+
+  var url = lozData.page;
+  var sep = '?';
+
+  for (var a in u) {
+    if (typeof u[a] == 'undefined')
+      continue;
+    url += sep + a + '=' + u[a];
+    sep = '&';
+  }
+
+  return url;
+}
+
+//}}}
+
+//{{{  lozGetInt
+
+function lozGetInt (key, def) {
+
+  for (var i=0; i < lozData.tokens.length; i++)
+    if (lozData.tokens[i] == key)
+      return parseInt(lozData.tokens[i+1]);
+
+  return def;
+}
+
+//}}}
+//{{{  lozGetInt1
+
+function lozGetInt1 (key, def) {
+
+  for (var i=0; i < lozData.tokens.length; i++)
+    if (lozData.tokens[i] == key)
+      return parseInt(lozData.tokens[i+2]);
+
+  return def;
+}
+
+//}}}
+//{{{  lozGetStr
+
+function lozGetStr (key, def) {
+
+  for (var i=0; i < lozData.tokens.length; i++)
+    if (lozData.tokens[i] == key)
+      return lozData.tokens[i+1];
+
+  return def;
+}
+
+//}}}
+//{{{  lozGetStrToEnd
+
+function lozGetStrToEnd (key, def) {
+
+  for (var i=0; i < lozData.tokens.length; i++) {
+    if (lozData.tokens[i] == key) {
+      var val = '';
+      for (var j=i+1; j < lozData.tokens.length; j++)
+        val += lozData.tokens[j] + ' ';
+      return val;
+    }
+  }
+
+  return def;
+}
+
+//}}}
+//{{{  lozStandardRx
+
+function lozStandardRx (e) {
+
+  //console.log(e.data);
+
+  lozData.message = e.data;
+  lozData.message = lozData.message.trim();
+  lozData.message = lozData.message.replace(/\s+/g,' ');
+  lozData.tokens  = lozData.message.split(' ');
+
+  //{{{  bestmove
+  
+  if (lozData.tokens[0] == 'bestmove') {
+  
+    lozUpdateStats();
+  
+    //console.log(lozData);
+  
+    lozData.bm   = lozGetStr('bestmove','');
+    lozData.bmFr = lozData.bm[0] + lozData.bm[1];
+    lozData.bmTo = lozData.bm[2] + lozData.bm[3];
+    if (lozData.bm.length > 4)
+      lozData.bmPr = lozData.bm[4];
+    else
+      lozData.bmPr = '';
+  
+    lozUpdateBestMove();
+  }
+  
+  //}}}
+  //{{{  option
+  
+  else if (lozData.tokens[0] == 'option') {
+    ;
+  }
+  
+  //}}}
+  //{{{  info string debug
+  
+  else if (lozData.tokens[0] == 'info' && lozData.tokens[1] == 'string' && lozData.tokens[2] == 'debug') {
+  
+    lozData.info = '<b>' + lozData.message.replace(/info string debug /,'') + '</b>';
+  
+    lozUpdateInfo();
+  }
+  
+  //}}}
+  //{{{  info string
+  
+  else if (lozData.tokens[0] == 'info' && lozData.tokens[1] == 'string') {
+  
+    lozData.info = lozData.message.replace(/info string /,'');
+  
+    lozUpdateInfo();
+  }
+  
+  //}}}
+  //{{{  info
+  
+  
+  else if (lozData.tokens[0] == 'info') {
+  
+    var pv    = lozData.pv;
+    var score = lozData.score;
+    var units = lozData.units;
+    var depth = lozData.depth;
+  
+    lozData.mvStr    = lozGetStr('currmove',lozData.mvStr);
+    lozData.mvNum    = lozGetInt('currmovenumber',lozData.mvNum);
+    lozData.depth    = lozGetInt('depth',lozData.depth);
+    lozData.selDepth = lozGetInt('seldepth',lozData.seldepth);
+    lozData.units    = lozGetStr('score',lozData.units);
+    lozData.score    = lozGetInt1('score',lozData.score);
+    lozData.pv       = lozGetStrToEnd('pv',lozData.pv);
+    lozData.nodes    = lozGetInt('nodes',lozData.nodes);
+    lozData.time     = lozGetInt('time',lozData.time);
+    lozData.nps      = lozGetInt('nps',lozData.nps);
+    lozData.hashFull = lozGetInt('hashfull',lozData.hashFull);
+  
+    lozData.seconds   = (lozData.time/1000).round(2);
+    lozData.meganodes = (lozData.nodes/1000000).round(2);
+    lozData.mnps      = (lozData.nps/1000000).round(2);
+    lozData.kilonodes = (lozData.nodes/1000).round(2);
+    lozData.knps      = (lozData.nps/1000).round(2);
+  
+    lozUpdateStats();
+  
+    if (pv != lozData.pv || score != lozData.score || units != lozData.units || depth != lozData.depth)
+      lozUpdatePV();
+  }
+  
+  //}}}
+  //{{{  board
+  
+  else if (lozData.tokens[0] == 'board') {
+  
+    lozData.board = lozGetStr('board','');
+  
+    lozUpdateBoard();
+  }
+  
+  //}}}
+  //{{{  everything else
+  
+  else {
+  
+    lozData.info = lozData.message;
+  
+    lozUpdateInfo();
+  }
+  
+  //}}}
+}
+
+//}}}
+//{{{  lozUpdateBestMove
+
+function lozUpdateBestMove () {
+
+}
+
+//}}}
+//{{{  lozUpdateStats
+
+function lozUpdateStats () {
+
+  if (lozData.mvNum && lozData.mvStr)
+    var move = ' | ' + lozData.mvNum + '/' + lozData.mvStr;
+  else
+    var move = '';
+
+  if (lozData.hashFull)
+    var hash = ' | ' + lozData.hashFull/10 + '%';
+  else
+    var hash = '';
+
+  $(lozData.idStats).html(lozData.seconds + ' s | ' + lozData.kilonodes + ' kn | ' + lozData.knps + ' kn/s' + move + hash);
+}
+
+//}}}
+//{{{  lozUpdatePV
+
+function lozUpdatePV () {
+
+  if (lozData.selDepth)
+    var d = lozData.depth + '/' + lozData.selDepth;
+  else
+    var d = lozData.depth;
+
+  $(lozData.idInfo).prepend(lozData.seconds + ' ' + d + ' (' + lozData.score + lozData.units + ') ' + lozData.pv + '<br>');
+}
+
+//}}}
+//{{{  lozUpdateInfo
+
+function lozUpdateInfo () {
+
+  $(lozData.idInfo).prepend(lozData.info + '<br>');
+}
+
+//}}}
+//{{{  lozUpdateBoard
+
+function lozUpdateBoard () {
+
+  var id = 'board' + lozData.next++;
+
+  $(lozData.idInfo).prepend('<div style="width: 250px;" id="' + id + '"><div>');
+
+  var cb = new ChessBoard(id, {
+    showNotation : true,
+    draggable    : true,
+    dropOffBoard : 'snapback',
+    position     : lozData.tokens[1]
+  });
+
+}
+
+//}}}
+
---
+++ play.js
@@ -0,0 +1,256 @@
+
+if (!window.Worker) {
+  document.write('<p><b>DUDE, YOUR BROWSER IS TOO OLD TO PLAY CHESS!<p>TRY <a href="http://www.google.co.uk/chrome/">GOOGLE CHROME</a></a><p>');
+  exit;
+}
+
+var args        = lozGetURLArgs();
+var board       = null;
+var chess       = null;
+var drag        = true;
+var engine      = null;
+var startFrom   = 'startpos';
+var startFromUI = 'start';
+
+lozData.page    = 'play.htm';
+lozData.idInfo  = '#info';
+lozData.idStats = '#stats';
+
+//{{{  lozUpdateBestMove
+
+function lozUpdateBestMove () {
+
+  var move = {};
+
+  move.from = lozData.bmFr;
+  move.to   = lozData.bmTo;
+
+  if (lozData.bmPr) {
+    move.promotion = lozData.bmPr;
+  }
+
+  chess.move(move);
+  board.position(chess.fen());
+  $('#moves').html(chess.pgn({newline_char: '<br>'}));
+
+  if (!chess.game_over())
+    drag = true;
+  else
+    showEnd();
+}
+
+//}}}
+//{{{  lozUpdatePV
+
+function lozUpdatePV () {
+
+  if (args.h != "y" && lozData.units == 'cp')
+    $(lozData.idInfo).prepend('depth ' + lozData.depth + ' (' + lozData.score + ') ' + lozData.pv + '<br>');
+  else if (lozData.score > 0 && lozData.units != 'cp')
+    $(lozData.idInfo).prepend('depth ' + lozData.depth + ' (<b>mate in ' + lozData.score + '</b>) ' + lozData.pv + '<br>');
+  else if (lozData.units != 'cp')
+    $(lozData.idInfo).prepend('depth ' + lozData.depth + ' (<b>checkmate</b>) ' + lozData.pv + '<br>');
+
+}
+
+//}}}
+//{{{  onDrop
+
+var onDrop = function(source, target, piece, newPos, oldPos, orientation) {
+
+  if (target == 'offboard' || target == source) {
+    //console.log('offboard');
+    return;
+  }
+
+  var move = chess.move({from: source, to: target, promotion: 'q'})
+  if (!move) {
+    //console.log('invalid');
+    return 'snapback';
+  }
+
+  if (move.flags == 'e' || move.flags == 'p' || move.flags == 'k' || move.flags == 'q')
+    board.position(chess.fen());
+
+  var pgn = chess.pgn({newline_char: '<br>'});
+  $('#moves').html(pgn);
+
+  drag = false;
+
+  if (!chess.game_over()) {
+    $(lozData.idInfo).html('');
+    var movetime = getMoveTime() * 1000;
+    engine.postMessage('position ' + startFrom + ' moves ' + strMoves());
+    if (args.m)
+      engine.postMessage(args.m);
+    else
+      engine.postMessage('go movetime ' + movetime);
+  }
+  else
+    showEnd();
+};
+
+//}}}
+//{{{  onDragStart
+
+var onDragStart = function(source, piece, position, orientation) {
+
+  if ((!drag || orientation === 'white' && piece.search(/^w/) === -1) || (orientation === 'black' && piece.search(/^b/) === -1)) {
+    return false;
+  }
+
+  return true;
+};
+
+//}}}
+//{{{  strMoves
+
+function strMoves() {
+
+  var movesStr = '';
+  var moves    = chess.history({verbose: true});
+
+  for (var i=0; i < moves.length; i++) {
+    if (i)
+      movesStr += ' ';
+    var move = moves[i];
+    movesStr += move.from + move.to;
+    if (move.promotion)
+      movesStr += move.promotion;
+  }
+
+  return movesStr;
+}
+
+//}}}
+//{{{  showEnd
+
+function showEnd () {
+
+  if (chess.in_checkmate())
+    $(lozData.idInfo).html('Checkmate');
+  else if (chess.insufficient_material())
+    $(lozData.idInfo).html('Draw due to insufficient material');
+  else if (chess.in_draw())
+    $(lozData.idInfo).html('Draw by 50 move rule');
+  else if (chess.in_stalemate())
+    $(lozData.idInfo).html('Draw by stalemate');
+  else if (chess.in_threefold_repetition())
+    $(lozData.idInfo).html('Draw by threefold repetition');
+  else
+    $(lozData.idInfo).html('Game over but not sure why!');
+}
+
+//}}}
+//{{{  getMoveTime
+
+function getMoveTime () {
+
+  var t = parseInt($('#permove').val());
+  if (t <= 0 || !t) {
+    t = 1;
+    $('#permove').val(1);
+  }
+  return t;
+}
+
+//}}}
+
+$(function() {
+
+  //{{{  init DOM
+  
+  if (args.t) {
+    $('#permove').val(args.t);
+    getMoveTime();
+  }
+  
+  $('input').tooltip({delay: {"show": 1000, "hide": 100}});
+  
+  //}}}
+  //{{{  handlers
+  
+  $('#playw').click(function() {
+  
+    window.location = lozMakeURL ({
+      t : getMoveTime()
+    });
+  
+    return true;
+  });
+  
+  $('#playb').click(function() {
+  
+    window.location = lozMakeURL ({
+      t : getMoveTime(),
+      c : 'b'
+    });
+  
+    return true;
+  });
+  
+  $('#analyse').click(function() {
+  
+    window.open("fen.htm?fen=" + chess.fen(),"_blank");
+  
+    return false;
+  });
+  
+  
+  //}}}
+
+  engine           = new Worker(lozData.source);
+  engine.onmessage = lozStandardRx;
+
+
+  if (args.fen) {
+    startFrom   = 'fen ' + args.fen;
+    startFromUI = args.fen;
+    chess = new Chess(args.fen);
+    $("#playw").hide();
+    $("#playb").hide();
+  }
+  else
+    chess = new Chess();
+
+  board = new ChessBoard('board', {
+    showNotation : true,
+    draggable    : true,
+    dropOffBoard : 'snapback',
+    onDrop       : onDrop,
+    onDragStart  : onDragStart,
+    position     : startFromUI
+  });
+
+  engine.postMessage('uci')
+  engine.postMessage('ucinewgame')
+  engine.postMessage('debug off')
+
+  if (!args.fen && args.c == 'b' || args.fen && args.fen.search(' w') !== -1 && args.c == 'b') {
+    board.orientation('black');
+    engine.postMessage('position ' + startFrom);
+    engine.postMessage('go movetime ' + getMoveTime() * 1000);
+    $(lozData.idInfo).prepend('You are black' + '<br>');
+  }
+  else if (!args.fen && args.c != 'b' || args.fen && args.fen.search(' w') !== -1 && args.c != 'b') {
+    board.orientation('white');
+    $(lozData.idInfo).prepend('Your move with white pieces' + '<br>');
+  }
+  else if (args.fen && args.fen.search(' b') !== -1 && args.c == 'b') {
+    board.orientation('black');
+    $(lozData.idInfo).prepend('Your move with black pieces' + '<br>');
+  }
+  else if (args.fen && args.fen.search(' b') !== -1 && args.c != 'b') {
+    board.orientation('white');
+    engine.postMessage('position ' + startFrom);
+    engine.postMessage('go movetime ' + getMoveTime() * 1000);
+    $(lozData.idInfo).prepend('You are white' + '<br>');
+  }
+  else {
+    $(lozData.idInfo).prepend('INCONSISTENT ARGS' + '<br>');
+  }
+
+  //console.log(args);
+
+});
+
