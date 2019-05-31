7a358b4f9fea5bdaaa711506dd35b5e990c97574
Colin Jenkins <op12no2@gmail.com>
2019-05-31 08:32:51 +0100

    Add files via upload

---
+++ ex.js
@@ -0,0 +1,12 @@
+
+var lozza = new Worker('lozza.js');
+
+lozza.onmessage = function (e) {
+  $('#dump').append(e.data + '<br>');
+};
+
+lozza.postMessage('uci');             // get build etc
+lozza.postMessage('ucinewgame');      // reset TT
+lozza.postMessage('position startpos');
+lozza.postMessage('go depth 10');     // 10 ply search
+
