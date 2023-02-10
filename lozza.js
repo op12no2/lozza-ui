4fa04c169ba63f2caa9b59d00ae99aa00e913335
Colin Jenkins <op12no2@gmail.com>
2023-02-10 08:04:31 +0000

    add tt to qs

--- lozza.js
+++ lozza.js
@@ -6,13 +6,17 @@
 
 var BUILD   = "2.5";
 var RELEASE = 1;
+var SILENT  = 0;
 
 //{{{  history
 /*
 
+2.5 09/02/23 Process command line args if invoked with Node.
+2.5 09/02/23 Add bench command using Ethereal FENs. Depth 9 nodes 4625388.
+2.5 09/02/23 Use TT in qsearch, but do not overwrite existing entries with depth > 0.
 2.5 05/02/23 Clear best move on fail low and don't allow timeout until there is a best move.
 2.5 03/02/23 Return/store bestScore not oAlpha on fail low in search.
-2.5 03/02/23 Increase/decrease alpha/beta on fail high/low duing ID.
+2.5 03/02/23 Increase/decrease alpha/beta on fail high/low during ID.
 2.5 03/02/23 Update best move on fail high.
 2.5 01/02/23 Round the pawn and attack values before the phase calculation. Retune.
 2.5 01/02/23 Don't use end game tempo. Retune.
@@ -54,6 +58,13 @@ else if ((typeof WorkerGlobalScope) == 'undefined') {
 
 function myround(x) {
   return Math.sign(x) * Math.round(Math.abs(x));
+}
+
+//}}}
+//{{{  docmd
+
+function docmd(x) {
+  onmessage({data: x});
 }
 
 //}}}
@@ -2018,11 +2029,6 @@ lozChess.prototype.alphabeta = function (node, depth, turn, alpha, beta, nullOK,
   
   if (depth <= 0) {
   
-    score = board.ttGet(node, 0, alpha, beta);
-  
-    if (score != TTSCORE_UNKNOWN)
-      return score;
-  
     score = this.qSearch(node, -1, turn, alpha, beta, 0);
   
     return score;
@@ -2045,12 +2051,12 @@ lozChess.prototype.alphabeta = function (node, depth, turn, alpha, beta, nullOK,
   var R         = 0;
   var E         = 0;
   var lonePawns = (turn == WHITE && board.wCount == board.wCounts[PAWN]+1) || (turn == BLACK && board.bCount == board.bCounts[PAWN]+1);
-  var standPat  = INFINITY;
+  var eval      = INFINITY;
   var doBeta    = !pvNode && !inCheck && !lonePawns && nullOK == NULL_Y && !board.betaMate(beta);
 
   //{{{  prune?
   
-  if (doBeta && depth <= 2 && ((standPat = board.getEval(standPat,node,turn)) - depth * 200) >= beta) {
+  if (doBeta && depth <= 2 && ((eval = board.getEval(eval,node,turn)) - depth * 200) >= beta) {
     return beta;
   }
   
@@ -2065,7 +2071,7 @@ lozChess.prototype.alphabeta = function (node, depth, turn, alpha, beta, nullOK,
   
   R = 3;
   
-  if (doBeta && depth > 2 && (standPat = board.getEval(standPat,node,turn)) > beta) {
+  if (doBeta && depth > 2 && (eval = board.getEval(eval,node,turn)) > beta) {
   
     board.loHash ^= board.loEP[board.ep];
     board.hiHash ^= board.hiEP[board.ep];
@@ -2107,7 +2113,7 @@ lozChess.prototype.alphabeta = function (node, depth, turn, alpha, beta, nullOK,
   var numSlides      = 0;
   var givesCheck     = INCHECK_UNKNOWN;
   var keeper         = false;
-  var doFutility     = !inCheck && depth <= 4 && ((standPat = board.getEval(standPat,node,turn)) + depth * 120) < alpha && !lonePawns;
+  var doFutility     = !inCheck && depth <= 4 && ((eval = board.getEval(eval,node,turn)) + depth * 120) < alpha && !lonePawns;
   var doLMR          = !inCheck && depth >= 3;
   var doLMP          = !pvNode && !inCheck && depth <= 2 && !lonePawns;
   var doIID          = !node.hashMove && pvNode && depth > 3;
@@ -2239,7 +2245,7 @@ lozChess.prototype.alphabeta = function (node, depth, turn, alpha, beta, nullOK,
 
         if (bestScore >= beta) {
           node.addKiller(bestScore, bestMove);
-          board.ttPut(TT_BETA, depth, bestScore, bestMove, node.ply, alpha, beta, standPat);
+          board.ttPut(TT_BETA, depth, bestScore, bestMove, node.ply, alpha, beta, eval);
           board.addHistory(depth*depth*depth, bestMove);
           return bestScore;
         }
@@ -2258,12 +2264,12 @@ lozChess.prototype.alphabeta = function (node, depth, turn, alpha, beta, nullOK,
   if (numLegalMoves == 0) {
   
     if (inCheck) {
-      //board.ttPut(TT_EXACT, depth, -MATE + node.ply, 0, node.ply, alpha, beta, standPat);
+      //board.ttPut(TT_EXACT, depth, -MATE + node.ply, 0, node.ply, alpha, beta, eval);
       return -MATE + node.ply;
     }
   
     else {
-      //board.ttPut(TT_EXACT, depth, CONTEMPT, 0, node.ply, alpha, beta, standPat);
+      //board.ttPut(TT_EXACT, depth, CONTEMPT, 0, node.ply, alpha, beta, eval);
       return CONTEMPT;
     }
   }
@@ -2271,11 +2277,11 @@ lozChess.prototype.alphabeta = function (node, depth, turn, alpha, beta, nullOK,
   //}}}
 
   if (bestScore > oAlpha) {
-    board.ttPut(TT_EXACT, depth, bestScore, bestMove, node.ply, alpha, beta, standPat);
+    board.ttPut(TT_EXACT, depth, bestScore, bestMove, node.ply, alpha, beta, eval);
     return bestScore;
   }
   else {
-    board.ttPut(TT_ALPHA, depth, bestScore, bestMove, node.ply, alpha, beta, standPat);
+    board.ttPut(TT_ALPHA, depth, bestScore, bestMove, node.ply, alpha, beta, eval);
     return bestScore;
   }
 }
@@ -2305,10 +2311,16 @@ lozChess.prototype.qSearch = function (node, depth, turn, alpha, beta, sq) {
   var board         = this.board;
   var numLegalMoves = 0;
   var move          = 0;
-  var standPat      = INFINITY;
+  var eval          = INFINITY;
   var phase         = 0;
   var nextTurn      = ~turn & COLOR_MASK;
   var to            = 0;
+
+  var score = board.ttGet(node, 0, alpha, beta);  // sets/clears node.hashMove and node.hashEval.
+
+  if (score != TTSCORE_UNKNOWN) {
+    return score;
+  }
 
   if (depth > -2)
     var inCheck = board.isKingAttacked(nextTurn);
@@ -2316,12 +2328,11 @@ lozChess.prototype.qSearch = function (node, depth, turn, alpha, beta, sq) {
     var inCheck = false;
 
   if (!inCheck) {
-    board.ttGet(node, depth, alpha, beta);
-    standPat = board.getEval(standPat, node, turn);
-    if (standPat >= beta)
-      return standPat;
-    if (standPat >= alpha)
-      alpha = standPat;
+    eval = board.getEval(eval, node, turn);
+    if (eval >= beta)
+      return eval;
+    if (eval >= alpha)
+      alpha = eval;
     phase = board.cleanPhase(board.phase);
   }
 
@@ -2341,7 +2352,7 @@ lozChess.prototype.qSearch = function (node, depth, turn, alpha, beta, sq) {
 
     //{{{  prune?
     
-    if (!inCheck && phase <= EPHASE && !(move & MOVE_PROMOTE_MASK) && standPat + 200 + MATERIAL[((move & MOVE_TOOBJ_MASK) >>> MOVE_TOOBJ_BITS) & PIECE_MASK] < alpha) {
+    if (!inCheck && phase <= EPHASE && !(move & MOVE_PROMOTE_MASK) && eval + 200 + MATERIAL[((move & MOVE_TOOBJ_MASK) >>> MOVE_TOOBJ_BITS) & PIECE_MASK] < alpha) {
     
       continue;
     }
@@ -2365,7 +2376,7 @@ lozChess.prototype.qSearch = function (node, depth, turn, alpha, beta, sq) {
 
     numLegalMoves++;
 
-    var score = -this.qSearch(node.childNode, depth-1, nextTurn, -beta, -alpha, (move & MOVE_TO_MASK) >>> MOVE_TO_BITS);
+    score = -this.qSearch(node.childNode, depth-1, nextTurn, -beta, -alpha, (move & MOVE_TO_MASK) >>> MOVE_TO_BITS);
 
     //{{{  unmake move
     
@@ -2377,6 +2388,7 @@ lozChess.prototype.qSearch = function (node, depth, turn, alpha, beta, sq) {
 
     if (score > alpha) {
       if (score >= beta) {
+        board.ttPut(TT_BETA, 0, beta, 0, node.ply, alpha, beta, eval);
         return score;
       }
       alpha = score;
@@ -2395,6 +2407,8 @@ lozChess.prototype.qSearch = function (node, depth, turn, alpha, beta, sq) {
   }
   
   //}}}
+
+  board.ttPut(TT_ALPHA, 0, alpha, 0, node.ply, alpha, beta, eval);
 
   return alpha;
 }
@@ -2418,6 +2432,8 @@ lozChess.prototype.perft = function () {
     var err = '';
   else
     var err = 'ERROR ' + error;
+
+  this.stats.nodes = moves;
 
   this.uci.send('info string',spec.id,spec.depth,moves,spec.moves,err,this.board.fen());
 }
@@ -5995,6 +6011,9 @@ lozBoard.prototype.ttPut = function (type,depth,score,move,ply,alpha,beta,eval) 
 
   var idx = this.loHash & TTMASK;
 
+  if (depth == 0 && this.ttType[idx] != TT_EMPTY && this.ttDepth[idx] > 0)
+    return;  // don't let qsearch tt entries overwrite search tt entries.
+
   //if (this.ttType[idx] == TT_EXACT && this.loHash == this.ttLo[idx] && this.hiHash == this.ttHi[idx] && this.ttDepth[idx] > depth && this.ttScore[idx] > alpha && this.ttScore[idx] < beta) {
     //return;
   //}
@@ -6861,9 +6880,24 @@ function lozUCI () {
 }
 
 //}}}
+//{{{  .argv
+
+lozUCI.prototype.argv = function () {
+
+  if (process.argv.length > 2) {
+    for (let i=2; i < process.argv.length; i++)
+      docmd(process.argv[i]);
+  }
+
+}
+
+//}}}
 //{{{  .post
 
 lozUCI.prototype.post = function (s) {
+
+  if (SILENT)
+    return;
 
   if (lozzaHost == HOST_NODEJS)
     this.nodefs.writeSync(1, s + '\n');
@@ -6960,6 +6994,64 @@ lozUCI.prototype.getArr = function (key, to) {
 
 //}}}
 //{{{  .onmessage
+
+//{{{  bench fens
+
+const BENCHFENS = [
+
+"r3k2r/2pb1ppp/2pp1q2/p7/1nP1B3/1P2P3/P2N1PPP/R2QK2R w KQkq a6 0 14",
+"4rrk1/2p1b1p1/p1p3q1/4p3/2P2n1p/1P1NR2P/PB3PP1/3R1QK1 b - - 2 24",
+"r3qbrk/6p1/2b2pPp/p3pP1Q/PpPpP2P/3P1B2/2PB3K/R5R1 w - - 16 42",
+"6k1/1R3p2/6p1/2Bp3p/3P2q1/P7/1P2rQ1K/5R2 b - - 4 44",
+"8/8/1p2k1p1/3p3p/1p1P1P1P/1P2PK2/8/8 w - - 3 54",
+"7r/2p3k1/1p1p1qp1/1P1Bp3/p1P2r1P/P7/4R3/Q4RK1 w - - 0 36",
+"r1bq1rk1/pp2b1pp/n1pp1n2/3P1p2/2P1p3/2N1P2N/PP2BPPP/R1BQ1RK1 b - - 2 10",
+"3r3k/2r4p/1p1b3q/p4P2/P2Pp3/1B2P3/3BQ1RP/6K1 w - - 3 87",
+"2r4r/1p4k1/1Pnp4/3Qb1pq/8/4BpPp/5P2/2RR1BK1 w - - 0 42",
+"4q1bk/6b1/7p/p1p4p/PNPpP2P/KN4P1/3Q4/4R3 b - - 0 37",
+"2q3r1/1r2pk2/pp3pp1/2pP3p/P1Pb1BbP/1P4Q1/R3NPP1/4R1K1 w - - 2 34",
+"1r2r2k/1b4q1/pp5p/2pPp1p1/P3Pn2/1P1B1Q1P/2R3P1/4BR1K b - - 1 37",
+"r3kbbr/pp1n1p1P/3ppnp1/q5N1/1P1pP3/P1N1B3/2P1QP2/R3KB1R b KQkq b3 0 17",
+"8/6pk/2b1Rp2/3r4/1R1B2PP/P5K1/8/2r5 b - - 16 42",
+"1r4k1/4ppb1/2n1b1qp/pB4p1/1n1BP1P1/7P/2PNQPK1/3RN3 w - - 8 29",
+"8/p2B4/PkP5/4p1pK/4Pb1p/5P2/8/8 w - - 29 68",
+"3r4/ppq1ppkp/4bnp1/2pN4/2P1P3/1P4P1/PQ3PBP/R4K2 b - - 2 20",
+"5rr1/4n2k/4q2P/P1P2n2/3B1p2/4pP2/2N1P3/1RR1K2Q w - - 1 49",
+"1r5k/2pq2p1/3p3p/p1pP4/4QP2/PP1R3P/6PK/8 w - - 1 51",
+"q5k1/5ppp/1r3bn1/1B6/P1N2P2/BQ2P1P1/5K1P/8 b - - 2 34",
+"r1b2k1r/5n2/p4q2/1ppn1Pp1/3pp1p1/NP2P3/P1PPBK2/1RQN2R1 w - - 0 22",
+"r1bqk2r/pppp1ppp/5n2/4b3/4P3/P1N5/1PP2PPP/R1BQKB1R w KQkq - 0 5",
+"r1bqr1k1/pp1p1ppp/2p5/8/3N1Q2/P2BB3/1PP2PPP/R3K2n b Q - 1 12",
+"r1bq2k1/p4r1p/1pp2pp1/3p4/1P1B3Q/P2B1N2/2P3PP/4R1K1 b - - 2 19",
+"r4qk1/6r1/1p4p1/2ppBbN1/1p5Q/P7/2P3PP/5RK1 w - - 2 25",
+"r7/6k1/1p6/2pp1p2/7Q/8/p1P2K1P/8 w - - 0 32",
+"r3k2r/ppp1pp1p/2nqb1pn/3p4/4P3/2PP4/PP1NBPPP/R2QK1NR w KQkq - 1 5",
+"3r1rk1/1pp1pn1p/p1n1q1p1/3p4/Q3P3/2P5/PP1NBPPP/4RRK1 w - - 0 12",
+"5rk1/1pp1pn1p/p3Brp1/8/1n6/5N2/PP3PPP/2R2RK1 w - - 2 20",
+"8/1p2pk1p/p1p1r1p1/3n4/8/5R2/PP3PPP/4R1K1 b - - 3 27",
+"8/4pk2/1p1r2p1/p1p4p/Pn5P/3R4/1P3PP1/4RK2 w - - 1 33",
+"8/5k2/1pnrp1p1/p1p4p/P6P/4R1PK/1P3P2/4R3 b - - 1 38",
+"8/8/1p1kp1p1/p1pr1n1p/P6P/1R4P1/1P3PK1/1R6 b - - 15 45",
+"8/8/1p1k2p1/p1prp2p/P2n3P/6P1/1P1R1PK1/4R3 b - - 5 49",
+"8/8/1p4p1/p1p2k1p/P2npP1P/4K1P1/1P6/3R4 w - - 6 54",
+"8/8/1p4p1/p1p2k1p/P2n1P1P/4K1P1/1P6/6R1 b - - 6 59",
+"8/5k2/1p4p1/p1pK3p/P2n1P1P/6P1/1P6/4R3 b - - 14 63",
+"8/1R6/1p1K1kp1/p6p/P1p2P1P/6P1/1Pn5/8 w - - 0 67",
+"1rb1rn1k/p3q1bp/2p3p1/2p1p3/2P1P2N/PP1RQNP1/1B3P2/4R1K1 b - - 4 23",
+"4rrk1/pp1n1pp1/q5p1/P1pP4/2n3P1/7P/1P3PB1/R1BQ1RK1 w - - 3 22",
+"r2qr1k1/pb1nbppp/1pn1p3/2ppP3/3P4/2PB1NN1/PP3PPP/R1BQR1K1 w - - 4 12",
+"2r2k2/8/4P1R1/1p6/8/P4K1N/7b/2B5 b - - 0 55",
+"6k1/5pp1/8/2bKP2P/2P5/p4PNb/B7/8 b - - 1 44",
+"2rqr1k1/1p3p1p/p2p2p1/P1nPb3/2B1P3/5P2/1PQ2NPP/R1R4K w - - 3 25",
+"r1b2rk1/p1q1ppbp/6p1/2Q5/8/4BP2/PPP3PP/2KR1B1R b - - 2 14",
+"6r1/5k2/p1b1r2p/1pB1p1p1/1Pp3PP/2P1R1K1/2P2P2/3R4 w - - 1 36",
+"rnbqkb1r/pppppppp/5n2/8/2PP4/8/PP2PPPP/RNBQKBNR b KQkq c3 0 2",
+"2rr2k1/1p4bp/p1q1p1p1/4Pp1n/2PB4/1PN3P1/P3Q2P/2RR2K1 w - f6 0 20",
+"3br1k1/p1pn3p/1p3n2/5pNq/2P1p3/1PN3PP/P2Q1PB1/4R1K1 w - - 0 23",
+"2r2b2/5p2/5k2/p1r1pP2/P2pB3/1P3P2/K1P3R1/7R w - - 23 93"
+];
+
+//}}}
 
 onmessage = function(e) {
 
@@ -7208,6 +7300,33 @@ onmessage = function(e) {
       
       //}}}
 
+    case 'bench':
+      //{{{  bench
+      
+      SILENT = 1;
+      
+      var nodes = 0;
+      
+      for (var i=0; i < BENCHFENS.length; i++) {
+      
+        var fen = BENCHFENS[i];
+      
+        docmd('ucinewgame');
+        docmd('position fen ' + fen);
+        docmd('id ' + i);
+        docmd('go depth 9');
+      
+        nodes += lozza.stats.nodes;
+      }
+      
+      SILENT = 0;
+      
+      uci.send('nodes', nodes);
+      
+      break;
+      
+      //}}}
+
     default:
       //{{{  ?
       
@@ -7249,6 +7368,8 @@ if (lozzaHost == HOST_NODEJS) {
   process.stdin.on('end', function() {
     process.exit();
   });
+
+  lozza.uci.argv();  // process command line args.
 }
 
 //}}}
