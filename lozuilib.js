b7c05f092482b828a718e7add3f8ca1214c4c3d7
Colin Jenkins <op12no2@gmail.com>
2025-06-05 12:37:55 +0100

    Update lozuilib.js

--- lozuilib.js
+++ lozuilib.js
@@ -9,7 +9,7 @@ lozData.next     = 0; // next board id.
 lozData.hashFull = 0;
 lozData.mvNum    = 0;
 lozData.mvStr    = '';
-lozData.source   = 'lozza51rel.js';
+lozData.source   = 'lozza.js';
 
 //{{{  Number.round
 
