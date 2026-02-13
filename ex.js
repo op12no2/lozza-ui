894290cfa916cc38724ff63c5edb1a88151a4e50
op12no2 <op12no2@gmail.com>
2026-02-13 09:22:06 +0000

    Change HTML line breaks to newline characters
    Updated line breaks in output from HTML line breaks to newline characters.

--- ex.js
+++ ex.js
@@ -5,11 +5,12 @@ const lozza = new Worker('lozza.js');
 const dump = document.getElementById('ucioutput');
 
 lozza.onmessage = function(e) {
-  ucioutput.textContent += e.data + '<br>'; // Lozza reponds with text as per UCI 
+  ucioutput.textContent += e.data + '\n'; // Lozza reponds with text as per UCI 
 };
 
 lozza.postMessage('uci');
 lozza.postMessage('ucinewgame');
 lozza.postMessage('position startpos');
 lozza.postMessage('go depth 10');
+
 
